79d102c0a75b9e95ed30b0ca93644ffab43586b8
James Carroll <jamcar23@gmail.com>
2021-01-06 12:34:46 -0600

    fix(ts): make app function return data type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
+function App(): React.ReactNode {
   return (
     <div className="App">
       <header className="App-header">
